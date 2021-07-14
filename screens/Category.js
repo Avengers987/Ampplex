e95a9aaa77b31f90658b68fa804edc966e8b95ab
Ankesh <ankesh3905222@gmail.com>
2021-07-14 14:16:53 +0530

    modified category.js

--- screens/Category.js
+++ screens/Category.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -8,93 +8,163 @@ import {
   TouchableOpacity,
   ScrollView,
   TextInput,
+  Image,
 } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-const Category = () => {
-  let FontSize = new Animated.Value(0);
-  useEffect(() => {
-    Animated.timing(FontSize, {
-      toValue: 20,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start();
-  }, [FontSize]);
+const RenderCategory = (name, setCategory) => {
+  return (
+    <TouchableOpacity
+      style={styles.Category}
+      onPress={() => setPressedBtn(setCategory === null ? name : setCategory)}
+    >
+      <Text style={styles.CategoryBtnInnerText}>{name}</Text>
+    </TouchableOpacity>
+  );
+};
 
-  console.log(FontSize);
+const Category = () => {
+  const [searchVal, setSearchVal] = useState(null);
+  const [pressedBtn, setPressedBtn] = useState(null);
+  const [btnColor, setBtnColor] = useState("powderblue");
+
+  console.log(searchVal);
+  console.log(pressedBtn);
 
   return (
     <View style={styles.container}>
-      <TextInput placeholder={"Search"} style={styles.searchBox}>
-        {require("../Images/2x/outline_search_black_24dp.png")}
-      </TextInput>
-      <Animated.View>
-        {/* {props.children} */}
+      <TextInput
+        placeholder={"Search"}
+        style={styles.searchBox}
+        onChangeText={(e) => setSearchVal(e)}
+        onFocus={() => console.log("Focused")}
+      />
+      <Image
+        source={require("../Images/2x/outline_search_black_24dp.png")}
+        style={styles.searchIcon}
+      />
+      <View>
         <Text style={styles.TitleStyle}>Select your category</Text>
-      </Animated.View>
+      </View>
 
       <ScrollView>
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>Coding {"</>"}</Text>
+        <TouchableOpacity
+          style={styles.Category}
+          onPress={() => setPressedBtn("Coding")}
+        >
+          <Text style={styles.CategoryBtnInnerText}>Coding {"</>"}</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>JEE</Text>
+        <TouchableOpacity
+          style={styles.Category}
+          onPress={() => setPressedBtn("JEE")}
+        >
+          <Text style={styles.CategoryBtnInnerText}>JEE</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>NEET</Text>
+        <TouchableOpacity
+          style={styles.Category}
+          onPress={() => setPressedBtn("NEET")}
+        >
+          <Text style={styles.CategoryBtnInnerText}>NEET</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>Sports</Text>
+        <TouchableOpacity
+          style={styles.Category}
+          onPress={() => setPressedBtn("Sports")}
+        >
+          <Text style={styles.CategoryBtnInnerText}>Sports</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>Mechanical</Text>
-          <Text numberOfLines={2} style={styles.CodingText}>
+
+        <TouchableOpacity
+          style={styles.Category}
+          onPress={() => setPressedBtn("Acting")}
+        >
+          <Text style={styles.CategoryBtnInnerText}>Acting</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.Category}
+          onPress={() => setPressedBtn("CBSE Class 1")}
+        >
+          <Text style={styles.CategoryBtnInnerText}>CBSE Class 1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.Category}
+          onPress={() => setPressedBtn("CBSE Class 2")}
+        >
+          <Text style={styles.CategoryBtnInnerText}>CBSE Class 2</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.Category}
+          onPress={() => setPressedBtn("CBSE Class 3")}
+        >
+          <Text style={styles.CategoryBtnInnerText}>CBSE Class 3</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.Category}
+          onPress={() => setPressedBtn("CBSE Class 4")}
+        >
+          <Text style={styles.CategoryBtnInnerText}>CBSE Class 4</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.Category}
+          onPress={() => setPressedBtn("CBSE Class 5")}
+        >
+          <Text style={styles.CategoryBtnInnerText}>CBSE Class 5</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.Category}
+          onPress={() => setPressedBtn("CBSE Class 6")}
+        >
+          <Text style={styles.CategoryBtnInnerText}>CBSE Class 6</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.Category}
+          onPress={() => setPressedBtn("CBSE Class 7")}
+        >
+          <Text style={styles.CategoryBtnInnerText}>CBSE Class 7</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.Category}
+          onPress={() => setPressedBtn("CBSE Class 8")}
+        >
+          <Text style={styles.CategoryBtnInnerText}>CBSE Class 8</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.Category}
+          onPress={() => setPressedBtn("CBSE Class 9")}
+        >
+          <Text style={styles.CategoryBtnInnerText}>CBSE Class 9</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.Category}
+          onPress={() => setPressedBtn("CBSE Class 10")}
+        >
+          <Text style={styles.CategoryBtnInnerText}>CBSE Class 10</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.Category}
+          onPress={() => setPressedBtn("CBSE Class 11")}
+        >
+          <Text style={styles.CategoryBtnInnerText}>CBSE Class 11</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.Category}
+          onPress={() => setPressedBtn("CBSE Class 12")}
+        >
+          <Text style={styles.CategoryBtnInnerText}>CBSE Class 12</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.Category}
+          onPress={() => setPressedBtn("Mechanical_Engineering")}
+        >
+          <Text style={styles.CategoryBtnInnerText}>Mechanical</Text>
+          <Text numberOfLines={2} style={styles.CategoryBtnInnerText}>
             Engineering
           </Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>Acting</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>CBSE Class 1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>CBSE Class 2</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>CBSE Class 3</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>CBSE Class 4</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>CBSE Class 5</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>CBSE Class 6</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>CBSE Class 7</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>CBSE Class 8</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>CBSE Class 9</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>CBSE Class 10</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>CBSE Class 11</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>CBSE Class 12</Text>
-        </TouchableOpacity>
 
         {/* None Button */}
-        <TouchableOpacity style={styles.Coding}>
-          <Text style={styles.CodingText}>None</Text>
+        <TouchableOpacity style={styles.Category}>
+          <Text style={styles.CategoryBtnInnerText}>None</Text>
         </TouchableOpacity>
       </ScrollView>
     </View>
@@ -115,7 +185,7 @@ const styles = StyleSheet.create({
     fontSize: 30,
     marginTop: Dimensions.get("window").width / 3.5,
   },
-  Coding: {
+  Category: {
     backgroundColor: "powderblue",
     width: 180,
     height: 50,
@@ -123,7 +193,7 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     borderRadius: 100,
   },
-  CodingText: {
+  CategoryBtnInnerText: {
     fontSize: 15,
     fontWeight: "bold",
     alignSelf: "center",
@@ -139,5 +209,12 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     paddingLeft: 120,
     fontSize: 16,
+  },
+  searchIcon: {
+    width: 30,
+    height: 30,
+    position: "absolute",
+    top: 60,
+    left: 75,
   },
 });
